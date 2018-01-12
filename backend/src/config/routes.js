5a7579771ebe03e67d7a976ceb13af95e1e7f80e
LuizHonorato <lhehonorato@gmail.com>
2018-01-12 17:37:28 -0200

    Offers

--- backend/src/config/routes.js
+++ backend/src/config/routes.js
@@ -1,16 +1,19 @@
 const express = require('express')
+const auth = require('./auth')
 
 module.exports = function(server) {
 
-    //URL base para todas as rotas do backend.
-    const router = express.Router()
-    server.use('/api', router)
+    //Rotas protegidas por JWT Token.
+    const protectedApi = express.Router()
+    server.use('/api', protectedApi)
+
+    protectedApi.use(auth)
 
     //Rota de cadastro de estabelecimentos
     const Store = require('../api/Store/StoreService')
-    Store.register(router, './stores')
+    Store.register(protectedApi, './stores')
 
     //Rota de cadastro de produtos
     const Product = require('../api/Product/ProductService')
-    Product.register(router, './products')
+    Product.register(protectedApi, './products')
 }
